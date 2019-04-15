eaefcb8c1514b4fcd92d9b84b7cf5f8bc2c12286
Tejas Cousik <tcousik0@gmail.com>
2019-04-15 17:23:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
     if number % 3 == 0 && number % 5 == 0
-      return "Fizzbuzz"
+      return "FizzBuzz"
     elsif number % 3 == 0
       return "Fizz"
     elsif number % 5 == 0

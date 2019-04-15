7c7648f3091ba23c214de58115c22245a0c3d2df
Tejas Cousik <tcousik0@gmail.com>
2019-04-15 17:22:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-    if number
+    if number % 3 == 0 && number % 5 == 0
+      return "Fizzbuzz"
+    elsif number % 3 == 0
+      return "Fizz"
+    elsif number % 5 == 0
+      return "Buzz"
+    else
+      return nil
 end
